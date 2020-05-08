d838b88ce71dafcbbcbdc4d49a9b20a59e4445f9
Ucizi Mafeni <ucizim@gmail.com>
2020-05-08 20:23:38 +0100

    add fix for more resilient link hinting

--- .surf/linkhint.js
+++ .surf/linkhint.js
@@ -291,3 +291,17 @@ function get_key(evt){
     }
     return ctrl+meta+key;
 }
+
+// fix for hints on problematic pages http://troubleshooters.com/linux/surf.htm
+function guarantee_stylesheet(){
+ var thehead = document.getElementsByTagName('head')[0];
+ if (document.getElementsByTagName('style').length < 1){
+    var thesty = document.createElement('style');
+    thesty.innerHTML = thesty.innerHTML + ' div#diag_kludge{color: #0099ff;} ';
+    thesty.innerHTML = thesty.innerHTML + ' div#diag_kludge{background-color: #660000;} ';
+    thehead.appendChild(thesty);
+    divv = document.getElementById('diag_kludge');
+    titl = headd.getElementsByTagName('title')[0];
+    }
+}
+window.addEventListener('load', guarantee_stylesheet, false);
