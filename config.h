28d6e743d01fcad16c0f9706810ae992f7004cba
Ucizi Mafeni <ucizim@gmail.com>
2020-06-19 21:29:21 +0100

    delete bindings.js

--- config.h
+++ config.h
@@ -4,7 +4,6 @@ static char *fulluseragent  = ""; /* Or override the whole user agent string */
 static char *scriptfiles[]  = {
 "~/Software/surf/.surf/scripts/script.js",
 "~/Software/surf/.surf/scripts/linkhint.js",
-"~/Software/surf/.surf/scripts/bindings.js"
 };
 static char *dldir          = "~/Downloads/";
 static char *dlstatus       = "~/.surf/dlstatus/";
